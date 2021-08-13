897a75a4d3b020a1f6654848988d2cfea972a349
Thomas <spym1933@foxmail.com>
2021-08-13 17:00:36 +0800

    全局组件ã<Nav/>

---
+++ src/views/Labels.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Labels.vue
+    <hr />
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Labels',
+
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/views/Statistics.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    Statistics.vue
+    <hr />
+    <Nav/>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Statistics',
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
