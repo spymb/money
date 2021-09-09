675c09fa7543d043d231cf401fc73d90477b26b3
Thomas <spym1933@foxmail.com>
2021-09-09 18:42:32 +0800

    引入全局变量，设置全局样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -24,6 +24,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
   nav {
     display: flex;
     box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
@@ -40,7 +41,7 @@ export default {
         height: 32px;
       }
     .item.selected {
-      color: red;
+      color: $color-highlight;
     }
   }
 </style>
