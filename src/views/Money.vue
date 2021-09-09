1c51c642b7391e3e588802e0780dfab028e42ba1
Thomas <spym1933@foxmail.com>
2021-09-09 20:56:43 +0800

    对记账页面的标签部分添加了样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -53,4 +53,31 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import "~@/assets/style/helper.scss";
+  .tags {
+    font-size: 14px;
+    padding: 16px;
+    > .current {
+      display: flex;
+      > li {
+        height: 24px;
+        line-height: 24px;
+        border-radius: 12px;
+        background: #d9d9d9;
+        padding: 0 16px;
+        margin-right: 12px;
+      }
+    }
+    > .new {
+      padding-top: 16px;
+      button {
+        background: transparent;
+        border: none;
+        color: #999;
+        border-bottom: 1px solid;
+        padding: 0 4px;
+      }
+    }
+  }
+
 </style>
