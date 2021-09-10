863907764e371cc8549ac649add0f08ca23d9d47
Thomas <spym1933@foxmail.com>
2021-09-10 09:40:50 +0800

    对Money的支出收入部分添加了样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -18,7 +18,7 @@
     </label>
 
     <div>
-      <ul class="type">
+      <ul class="inAndout">
         <li class="selected">支出</li>
         <li>收入</li>
       </ul>
@@ -97,5 +97,30 @@ export default {
       padding-right: 16px;
     }
   }
+  .inAndout {
+    display: flex;
+    background: #c4c4c4;
+    /*text-align: center;*/
+    font-size: 24px;
+    > li {
+      width: 50%;
+      height: 64px;
+      /*line-height: 64px;*/
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: relative;
+      &.selected::after {
+        content: '';
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: #333;
+      }
+    }
+  }
+
 
 </style>
