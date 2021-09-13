06b42446b1a38274aaa46affa49def3e4a50ddc0
Thomas <spym1933@foxmail.com>
2021-09-13 18:30:04 +0800

    运用MVC的思想分离出一个model.ts

---
+++ src/custom.d.ts
@@ -0,0 +1,8 @@
+// TS声明一个类型
+type RecordItem = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createdAt?: Date;
+}
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,9 +1,9 @@
 <template>
   <Layout>
-    {{ recordList }}
+    {{ recordItemList }}
     <!-- 监听各组件的更新事件，如事件被触发就执行对应的回调函数 -->
-    <Calculator :value.sync="record.amount" @submit="saveRecord" />
-    <Types :value.sync="record.type" />
+    <Calculator :value.sync="recordItem.amount" @submit="saveRecordItem" />
+    <Types :value.sync="recordItem.type" />
     <Notes @update:value="onUpdateNotes" />
     <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
   </Layout>
@@ -16,51 +16,44 @@ import Types from "@/components/Money/Types.vue";
 import Notes from "@/components/Money/Notes.vue";
 import Tags from "@/components/Money/Tags.vue";
 import { Component, Watch } from "vue-property-decorator";
-
-// TS声明一个类型
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-  createdAt?: Date;
-};
+import model from "@/model";
 
 @Component({
   components: { Tags, Notes, Types, Calculator },
 })
 export default class Money extends Vue {
   tags = ["衣", "食", "住", "行"];
-  recordList: Record[] = JSON.parse(
-    window.localStorage.getItem("recordList") || "[]"
-  );
-  // 用声明的Record类型收集各组件数据，首先设置默认值
+
+  // 用声明的RecordItem类型收集各组件数据，首先设置默认值
   // 一开始什么都没收集当然应该为空啦
-  record: Record = {
+  recordItem: RecordItem = {
     tags: [],
     notes: "",
     type: "-",
     amount: 0,
   };
-  // 下面的回调函数表示组件数据更新时，把更新的数据存入record
+
+  recordItemList = model.fetch();
+
+  // 下面的回调函数表示组件数据更新时，把更新的数据存入RecordItem
   onUpdateNotes(value: string) {
-    this.record.notes = value;
+    this.recordItem.notes = value;
   }
 
   onUpdateTags(value: string[]) {
-    this.record.tags = value;
+    this.recordItem.tags = value;
   }
 
-  saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
-    record2.createdAt = new Date();
-    this.recordList.push(record2);
-    console.log(this.recordList);
+  saveRecordItem() {
+    const recordItem2: RecordItem = model.clone(this.recordItem);
+    recordItem2.createdAt = new Date();
+    this.recordItemList.push(recordItem2);
+    console.log(this.recordItemList);
   }
 
-  @Watch("recordList")
-  onRecordListChanged() {
-    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+  @Watch("recordItemList")
+  onRecordItemListChanged() {
+    model.save(this.recordItemList);
   }
 }
 </script>
