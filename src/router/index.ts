7f781b96614c5ca207fe946114b8b7f07c6277bf
Thomas <spym1933@foxmail.com>
2021-09-19 15:53:56 +0800

    标签页的新建标签功能存在bug

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import VueRouter, { RouteConfig } from 'vue-router'
+import Vue from 'vue';
+import VueRouter, {RouteConfig} from 'vue-router';
 //import Home from '@/views/Home.vue'
-import Money from '@/views/Money.vue'
-import Labels from '@/views/Labels.vue'
-import Statistics from '@/views/Statistics.vue'
+import Money from '@/views/Money.vue';
+import Labels from '@/views/Labels.vue';
+import Statistics from '@/views/Statistics.vue';
 import NotFound from '@/views/NotFound.vue';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
@@ -29,10 +29,10 @@ const routes: Array<RouteConfig> = [
     path: '*',
     component: NotFound
   },
-]
+];
 
 const router = new VueRouter({
   routes
-})
+});
 
-export default router
+export default router;
