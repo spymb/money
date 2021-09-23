b4cbfa867e4b2bbd00dbe21fbf51318f74a69301
spymb <spym1933@foxmail.com>
2021-09-23 10:53:11 +0800

    封装出一个model

---
+++ src/model.ts
@@ -0,0 +1,14 @@
+const localStorageKeyname = 'recordList';
+const model = {
+  clone(data: RecordItem[] | RecordItem) {
+    return JSON.parse(JSON.stringify(data))
+  },
+  fetch() {
+    return JSON.parse(window.localStorage.getItem('localStorageKeyname') || '[]') as RecordItem[];
+  },
+  save(data: RecordItem[]) {
+    window.localStorage.setItem('localStorageKeyname', JSON.stringify(data));
+  }
+};
+
+export default model;
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,47 +1,40 @@
 <template>
   <Layout class-prefix="xxx">
     <!-- 监听各组件的更新事件，如事件被触发就执行对应的回调函数 -->
-    <Calculator :value.sync="record.amount" @submit="saveRecord" />
-    <Types :value.sync="record.type" />
-    <Notes @update:value="onUpdateNotes" />
-    <Tags :data-source.sync="tags" @update:value="onUpdateTags" />
+    <Calculator :value.sync="record.amount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
     {{ recordList }}
   </Layout>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-import Calculator from "@/components/Money/Calculator.vue";
-import Types from "@/components/Money/Types.vue";
-import Notes from "@/components/Money/Notes.vue";
-import Tags from "@/components/Money/Tags.vue";
-import { Component, Watch } from "vue-property-decorator";
+import Vue from 'vue';
+import Calculator from '@/components/Money/Calculator.vue';
+import Types from '@/components/Money/Types.vue';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
+import {Component, Watch} from 'vue-property-decorator';
+import model from '@/model';
 
-// TS声明一个类型
-type Record = {
-  tags: string[];
-  notes: string;
-  type: string;
-  amount: number;
-  createdAt?: Date;
-};
+const recordList = model.fetch();
 
 @Component({
-  components: { Tags, Notes, Types, Calculator },
+  components: {Tags, Notes, Types, Calculator},
 })
 export default class Money extends Vue {
-  tags = ["衣", "食", "住", "行"];
-  recordList: Record[] = JSON.parse(
-    window.localStorage.getItem("recordList") || "[]"
-  );
+  tags = ['衣', '食', '住', '行'];
+  recordList: RecordItem[] = recordList;
   // 用声明的Record类型收集各组件数据，首先设置默认值
   // 一开始什么都没收集当然应该为空啦
-  record: Record = {
+  record: RecordItem = {
     tags: [],
-    notes: "",
-    type: "-",
+    notes: '',
+    type: '-',
     amount: 0,
   };
+
   // 下面的回调函数表示组件数据更新时，把更新的数据存入record
   onUpdateNotes(value: string) {
     this.record.notes = value;
@@ -52,15 +45,15 @@ export default class Money extends Vue {
   }
 
   saveRecord() {
-    const record2: Record = JSON.parse(JSON.stringify(this.record));
+    const record2: RecordItem = model.clone(this.record);
     record2.createdAt = new Date();
     this.recordList.push(record2);
     console.log(this.recordList);
   }
 
-  @Watch("recordList")
+  @Watch('recordList')
   onRecordListChanged() {
-    window.localStorage.setItem("recordList", JSON.stringify(this.recordList));
+    model.save(this.recordList);
   }
 }
 </script>
