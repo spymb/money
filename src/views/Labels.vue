f228b69556752ce2f0757065ecc52ff682ca8cca
spymb <spym1933@foxmail.com>
2021-09-25 01:03:16 +0800

    点击标签可跳转至编辑页面

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,11 +1,12 @@
 <template>
   <Layout>
-    <ol class="tags">
-      <li v-for="tag in tags" :key="tag.id">
+    <div class="tags">
+      <router-link v-for="tag in tags" :key="tag.id"
+      :to="`/labels/edit/${tag.id}`" class="tag">
         <span>{{ tag.name }}</span>
         <Icon name="right"/>
-      </li>
-    </ol>
+      </router-link>
+    </div>
     <div class="createTag-wrapper">
       <button class="createTag" @click="createTag">新建标签</button>
     </div>
@@ -40,7 +41,7 @@ export default class Labels extends Vue {
   font-size: 16px;
   padding-left: 16px;
 
-  > li {
+  > .tag {
     min-height: 44px;
     display: flex;
     align-items: center;
