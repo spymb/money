4fe676e73f87cd33087666da95b78b4f2dfccf09
spymb <spym1933@foxmail.com>
2021-09-29 14:39:28 +0800

    value从data变成了prop

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -5,9 +5,9 @@
     </div>
 
     <ul class="current">
-      <li v-for="tag in dataSource" :key="tag" @click="toggle(tag)"
-          :class="{selected: selectedTags.indexOf(tag) >= 0}">
-        {{ tag }}
+      <li v-for="tag in dataSource" :key="tag.id"
+          :class="{selected: selectedTags.indexOf(tag) >= 0}"
+          @click="toggle(tag)" > {{ tag.name }}
       </li>
     </ul>
   </div>
