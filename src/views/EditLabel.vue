2a1744596cbde91f99e3228593667046e762d092
spymb <spym1933@foxmail.com>
2021-09-30 19:57:36 +0800

    展示tag的name

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -7,7 +7,7 @@
     </div>
 
     <div class="form-wrapper">
-      <FormItem field-name="标签名" placeholder="请输入标签名"/>
+      <FormItem :value="tag.name" field-name="标签名" placeholder="请输入标签名"/>
     </div>
 
     <div class="button-wrapper">
@@ -22,17 +22,20 @@ import {Component} from 'vue-property-decorator';
 import tagListModel from '@/models/tagListModel';
 import FormItem from '@/components/Money/FormItem.vue';
 import Button from '@/components/Button.vue';
+
 @Component({
   components: {Button, FormItem}
 })
 export default class EditLabel extends Vue {
+  tag?: { name: string, id: string } = undefined;
+
   created() {
     const id = this.$route.params.id;
     tagListModel.fetch();
     const tags = tagListModel.data;
     const tag = tags.filter(t => t.id === id)[0];
     if (tag) {
-      console.log(tag);
+      this.tag = tag;
     } else {
       this.$router.replace('/404');
     }
@@ -41,28 +44,32 @@ export default class EditLabel extends Vue {
 </script>
 
 <style lang="scss" scoped>
- .nav-bar {
-   text-align: center;
-   font-size: 16px;
-   background: white;
-   padding: 12px 16px;
-   display: flex;
-   align-items: center;
-   justify-content: space-between;
-   > .icon-left {
-     width: 24px;
-     height: 24px;
-   }
-   > .title {
+.nav-bar {
+  text-align: center;
+  font-size: 16px;
+  background: white;
+  padding: 12px 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
 
-   }
-   > .icon-right {
-     width: 24px;
-     height: 24px;
-   }
- }
- .form-wrapper {
-   background: white;
-   margin-top: 8px;
- }
+  > .icon-left {
+    width: 24px;
+    height: 24px;
+  }
+
+  > .title {
+
+  }
+
+  > .icon-right {
+    width: 24px;
+    height: 24px;
+  }
+}
+
+.form-wrapper {
+  background: white;
+  margin-top: 8px;
+}
 </style>
