a719d2da4731dcea573c7ad0bac3bfcc4f6bd646
spymb <spym1933@foxmail.com>
2021-10-02 15:10:01 +0800

    修复了一些小问题

--- src/lib/createID.ts
+++ src/lib/createID.ts
@@ -2,6 +2,7 @@ let id: number = parseInt(window.localStorage.getItem('_idMax') || '0') || 0;
 
 function createID() {
   id++;
+  window.localStorage.setItem('id_max', id.toString());
   return id;
 }
 
