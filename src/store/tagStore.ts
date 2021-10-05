ae69b36b9f59aa244ed42f60d68cb9884025c72e
spymb <spym1933@foxmail.com>
2021-10-05 21:36:06 +0800

    将model融合进store

---
+++ src/store/tagStore.ts
@@ -0,0 +1,60 @@
+import createID from '@/lib/createID';
+
+const localStorageKeyname = 'tagList';
+
+const tagStore = {
+  tagList: [] as Tag[],
+  fetchTags() {
+    this.tagList = JSON.parse(window.localStorage.getItem('localStorageKeyname') || '[]');
+    return this.tagList;
+  },
+  createTag(name: string) {
+    const names = this.tagList.map(item => item.name);
+    if (names.indexOf(name) >= 0) {
+      window.alert('标签名重复');
+      return 'duplicated';
+    }
+    const id = createID().toString();
+    this.tagList.push({id, name: name});
+    this.saveTags();
+    return 'success';
+  },
+  removeTag(id: string) {
+    let index = -1;
+    for (let i = 0; i < this.tagList.length; i++) {
+      if (this.tagList[i].id === id) {
+        index = i;
+        break;
+      }
+    }
+    this.tagList.splice(index, 1);
+    this.saveTags();
+    return true;
+  },
+  updateTag(id: string, name: string) {
+    const idList = this.tagList.map(item => item.id);
+    if (idList.indexOf(id) >= 0) {
+      const names = this.tagList.map(item => item.name);
+      if (names.indexOf(name) >= 0) {
+        return 'duplicated';
+      } else {
+        const tag = this.tagList.filter(item => item.id === id)[0];
+        tag.name = name;
+        this.saveTags();
+        return 'success';
+      }
+    } else {
+      return 'not found';
+    }
+  },
+  findTag(id: string) {
+    return this.tagList.filter(t => t.id === id)[0];
+  },
+  saveTags() {
+    window.localStorage.setItem('localStorageKeyname', JSON.stringify(this.tagList));
+  }
+};
+
+tagStore.fetchTags();
+
+export default tagStore;
