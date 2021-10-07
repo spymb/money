5832d9603d433bfd9ac78651aacb14f3e9d46e07
spymb <spym1933@foxmail.com>
2021-10-07 16:08:48 +0800

    Labels.vue使用Vuex，抽离TagHelper作为mixin

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="tags">
     <div class="new">
-      <button @click="create">新增标签</button>
+      <button @click="createTag">新增标签</button>
     </div>
 
     <ul class="current">
@@ -14,8 +14,9 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import {mixins} from 'vue-class-component';
+import TagHelper from '@/mixins/TagHelper';
 
 @Component({
   computed: {
@@ -24,14 +25,13 @@ import {Component} from 'vue-property-decorator';
     }
   }
 })
-export default class Tags extends Vue {
+export default class Tags extends mixins(TagHelper) {
   selectedTags: string[] = [];
 
   created() {
     this.$store.commit('fetchTags');
   }
 
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   toggle(tag: string) {
     const index = this.selectedTags.indexOf(tag);
     if (index >= 0) {
@@ -41,15 +41,6 @@ export default class Tags extends Vue {
     }
     // toggle标签时触发更新事件，并将this.selectedTags作为参数传给事件被触发时执行的回调函数
     this.$emit('update:value', this.selectedTags);
-  }
-
-  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  create() {
-    const name = window.prompt('请输入标签名');
-    if (!name) {
-      return window.alert('标签名不能为空');
-    }
-    this.$store.commit('createTag', name);
   }
 }
 </script>
--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -14,23 +14,27 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Button from '@/components/Button.vue';
+import TagHelper from '@/mixins/TagHelper';
+import {mixins} from 'vue-class-component';
 
 @Component({
-  components: {Button}
+  components: {Button},
+  computed: {
+    tags() {
+      return this.$store.state.tagList;
+    }
+  }
 })
-export default class Labels extends Vue {
-  // TODO
-  tags = [];
-
+export default class Labels extends mixins(TagHelper) {
+  created() {
+    this.$store.commit('fetchTags');
+  }
   createTag() {
     const name = window.prompt('请输入标签名');
-    if (name) {
-      // TODO
-      //store.createTag(name)
-    }
+    if (!name) {return window.alert('标签名不能为空');}
+    this.$store.commit('createTag', name);
   }
 }
 </script>
