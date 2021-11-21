57e62b208bc9441a946be9ca02270d0708b27785
spymb <spym1933@foxmail.com>
2021-11-21 13:07:29 +0800

    完成列表页CSS

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,45 +2,94 @@
   <Layout>
     <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
-    <div>
-      type: {{type}}
-      <br/>
-      interval: {{interval}}
-    </div>
+    <ol>
+      <li v-for="(group, index) in result" :key="index">
+        <h3 class="title">{{ group.title }}</h3>
+        <ol>
+          <li v-for="item in group.items" :key="item.id" class="record">
+            <span>{{ tagString(item.tags) }}</span>
+            <span class="notes">{{ item.notes }}</span>
+            <span>￥{{ item.amount }}</span>
+          </li>
+        </ol>
+      </li>
+    </ol>
   </Layout>
 </template>
 
 <script lang="ts">
-  import Vue from 'vue';
-  import {Component} from 'vue-property-decorator';
-  import Tabs from '@/components/Tabs.vue';
-  import intervalList from '@/constants/intervalList';
-  import typeList from '@/constants/typeList';
+import Vue from 'vue';
+import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constants/intervalList';
+import typeList from '@/constants/typeList';
 
-  @Component({
-    components: {Tabs},
-  })
-  export default class Statistics extends Vue {
-    type = '-';
-    interval = 'day';
-    intervalList = intervalList;
-    typeList = typeList;
+@Component({
+  components: {Tabs},
+})
+export default class Statistics extends Vue {
+  tagString(tags: Tag[]) {
+    return tags.length === 0 ? '无' : tags.join(',')
   }
+  get recordList() {
+    return (this.$store.state as RootState).recordList;
+  }
+  get result() {
+    const {recordList} = this;
+    type HashTableValue = { title: string, items: RecordList[] };
+    type HashTable = { [key: string]: HashTableValue }
+    const hashTable: HashTable = {};
+
+    for (let i = 0; i < recordList.length; i++) {
+      const [date, time] = recordList[i].createdAt!.split('T');
+      hashTable[date] = hashTable[date] || {title: date, items: []};
+      hashTable[date].items.push(recordList[i]);
+    }
+    return hashTable;
+  }
+
+  type = '-';
+  interval = 'day';
+  intervalList = intervalList;
+  typeList = typeList;
+}
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep {
-    .type-tabs-item {
-      background: white;
-      &.selected {
-        background: #c4c4c4;
-        &::after {
-          display: none;
-        }
+::v-deep {
+  .type-tabs-item {
+    background: white;
+
+    &.selected {
+      background: #c4c4c4;
+
+      &::after {
+        display: none;
       }
     }
-    .interval-tabs-item {
-      height: 48px;
-    }
   }
+
+  .interval-tabs-item {
+    height: 48px;
+  }
+}
+%item {
+  padding: 8px 16px;
+  line-height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-content: center;
+}
+.title {
+  @extend %item;
+}
+.record {
+  background: white;
+  @extend %item;
+}
+.notes {
+  margin-right: auto;
+  margin-left: 16px;
+  color: #999;
+}
 </style>
