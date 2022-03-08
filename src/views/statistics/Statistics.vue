1eb4660e9177423ec71cac2026f0c22f6385a5fe
spymb <spym1933@foxmail.com>
2022-03-08 23:16:50 +0800

    完成Statistics页面dateTypeSelector

--- src/views/statistics/Statistics.vue
+++ src/views/statistics/Statistics.vue
@@ -1,19 +1,48 @@
 <template>
   <Layout>
-    <div>statistics</div>
+    <div class="dateTypeSelector">
+      <Tabs :data-source="dateTypes" :value.sync="selectedDateType"/>
+    </div>
   </Layout>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
+import Tabs from '@/components/Tabs.vue';
 
-@Component
+@Component({
+  components: {Tabs}
+})
 export default class Statistics extends Vue {
-
+  dateTypes = [
+    {text: '月', value: 'year-month'},
+    {text: '年', value: 'year'},
+  ];
+  selectedDateType = 'year-month';
 }
 </script>
 
 <style lang="scss" scoped>
+@import "src/assets/style/helper";
 
+::v-deep .dateTypeSelector {
+  .tabs {
+    box-shadow: inset 0 -5px 5px -5px rgba(0, 0, 0, 0.25),
+    inset 0 5px 5px -5px rgba(0, 0, 0, 0.25);
+    display: flex;
+    background: white;
+
+    &-item {
+      width: 50%;
+      text-align: center;
+      padding: 16px 0;
+
+      &.selected {
+        background: $mainColor;
+        color: white;
+      }
+    }
+  }
+}
 </style>
