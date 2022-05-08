e1f886e84f97dd0b1a64b1ce2bb109cbaa71db62
spymb <spym1933@foxmail.com>
2022-05-08 11:06:48 +0800

    更新部署文件

--- deploy.sh
+++ deploy.sh
@@ -1,25 +1,10 @@
-#!/usr/bin/env sh
-
-# 当发生错误时中止脚本
-set -e
-
-# 构建
-yarn build
-
-# cd 到构建输出的目录下
-cd dist
-
-# 部署到自定义域域名
-# echo 'www.example.com' > CNAME
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@gitee.com:spym404/v-money.git master
-
+rm -rf dist &&
+yarn build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M master &&
+git remote add origin git@gitee.com:spym404/vui-website.git &&
+git push -f -u origin master &&
 cd -
